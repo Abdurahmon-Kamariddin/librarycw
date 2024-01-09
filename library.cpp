aafbb290fd8993720d2bfb6af51383c030191d6e
Abdurahmon-Kamariddin <ak2527@live.mdx.ac.uk>
2024-01-09 17:15:55 +0000

    Reading and parsing .csv file finally successful

--- library.cpp
+++ library.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <string>
+#include <locale>
 #include <vector>
+#include <ctime>
+#include <filesystem>
 #include <fstream>
+#include <sstream>
+
 using namespace std;
-//We list our members in a vector that we only allow upto 200 members
-vector<Member> members;
+using namespace filesystem;
+
 //Our maximum number of members allowed in our library
 int MAX_MEMBERS = 200;
 //Each time we add a member we increment this counter (we check against this counter every time we add to make sure membercount is < MAX_MEMBERS)
-int memberCount = 0;
+int memberCount = 1;
+
+
 //Classes
 class Book; // Forward declaration to break cyclic dependancy for member class
 class Date{
@@ -93,10 +100,10 @@ public:
 
 class Member : public Person
 {
-public:
-  friend class Book;
+private:
   int memberID;
   vector<Book> booksLoaned; //vector of book IDs which are the names of each book object
+public:
 
   Member(int newmemberID): memberID(newmemberID){}
 
@@ -114,7 +121,7 @@ public:
 
 class Book{
 public:
-  friend class Member;
+  int bookPages;
   int bookID;
   string bookName;
   string authorFirstName;
@@ -145,8 +152,8 @@ public:
     return dueDate;
   }
 
-  void setDueDate(Date dueDate){
-    dueDate = dueDate;
+  void setDueDate(Date date){
+    dueDate = date;
   }
 
   void returnBook(){
@@ -202,23 +209,77 @@ public:
   }
 };
 
-
 int main(){
-  string name;
-  string address;
-  string email;
+  //We declare the extension we are looking for as we will not hardcode the name of data file
+  string datafileext = ".csv";
+  //
+  for(auto i : directory_iterator(current_path())){ //iterating over each file in the directory
+    if(i.path().extension() == datafileext){ // if that file has a csv extension we open that file (our limitation is that only one csv file can be in the directory for the program to work
+      ifstream bookDataFile(i.path());
+      if (bookDataFile.is_open()){
+	string line;
+	getline(bookDataFile,line); //Skip the header line with table titles
+	int bID, bPageCount;
+	string idStr, bName, pageStr , bAuthFName, bAuthLName, bType;
+	vector<Book> allbooks;
+        while(getline(bookDataFile,line)){
+	  string trash; //Just a variable to hold double quote or any spaces that are left behind
+	  stringstream ss(line);
+	  getline(ss, idStr, ','); // Reading in id for book
+	  if(line.at(idStr.length() + 1) == '"'){
+	    cout << "1idStr passed into stoi is: " << idStr << endl;
+	    bID = stoi(idStr); //Read in id as string then converted to int
+	    getline(ss, trash, '"'); 
+	    getline(ss, bName, '"');
+	    getline(ss, trash, ',');
+	    getline(ss, pageStr, ',');
+	    cout << "1pageStr passed into stoi is: " << pageStr << endl;
+	    bPageCount = stoi(pageStr);
+	    getline(ss, bAuthFName, ',');
+	    getline(ss, bAuthLName, ',');
+	    getline(ss, bType, ',');
+	  }else{
+	    cout << "2idStr passed into stoi is: " << idStr << endl;
+	    bID = stoi(idStr); //Read in id as string then converted to int
+	    getline(ss, bName, ',');//Reading in the name
+	    getline(ss, pageStr, ',');
+	    cout << "2pageStr passed into stoi is: " << pageStr << endl;
+	    bPageCount = stoi(pageStr);
+	    getline(ss, bAuthFName, ',');
+	    getline(ss, bAuthLName, ',');
+	    getline(ss, bType, ',');
+	  }
+	  cout << bID << " , " << bName << " , " << bPageCount << " , " << bAuthFName << " , " << bAuthLName << " , " << bType << endl;
+	}
+	bookDataFile.close();
+	cout << "SUCCESS";
+      }else{
+	cout << "Could not open file : " << i.path();
+	return 0;
+      }
+    }
+  }
   
+  string staffName;
+  string staffAddress;
+  string staffEmail;
+  int baseSalary = 25000;
+  int id;
   cout << "Welcome fellow librarian to the Kamariddin library! \n";
   cout << "Please enter your name to create a staff account. \n";
-  cin << ;
-  Librarian * main = new Librarian(196, 20000);
-  abdurahmon->name = "Abdurahmon";
-  abdurahmon->address =  "Gatliff Road";
-  abdurahmon->email = "ak2527@live.mdx.ac.uk";
-  cout <<  abdurahmon->getName();
-  for(int i=0; i<MAX_MEMBERS; i++){
-    members[i] = (new Member(i));
-  }
+  getline(cin,staffName);
+  cout << "Please enter your address. \n";
+  getline(cin,staffAddress);
+  cout << "Please enter your email. \n";
+  getline(cin,staffEmail);
+  id = (rand() * 9000) + 1000;
+  Librarian * main = new Librarian(id, baseSalary);
+  main->name = staffName;
+  main->address =  staffAddress;
+  main->email = staffEmail;
+  main->setSalary(baseSalary);
+  cout <<  "Welcome to the Library management program " << main->getName() << "#" << main->getStaffID() << ". \n";
+  cout << "Your details are as follows - \n" << "Address: " << main->getAddress() << endl << "Email: " << main->getEmail() << endl << "Base salary: " << main->getSalary() << endl;
   
   return 0;
 }
